9114e06270df47e3f72fbb2a9279a0dc51a59da2
Dan Kram <dan@gnawmark.com>
2020-04-03 23:28:30 -0700

    Move away from getInitialProps in static version

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,24 @@
+import { useEffect, useState } from 'react'
 import api from '../api'
 import get from 'lodash/get'
-import PropTypes from 'prop-types'
-import { listShips, listCharacters } from '../graphql/queries'
+import { listShips } from '../graphql/queries'
 
-const HomePage = ({ ships, characters }) => {
+const HomePage = () => {
+  const [ships, setShips] = useState([])
+
+  useEffect(async () => {
+    const shipData = await api(listShips)
+    setShips(get(shipData, 'data.listShips.items', []))
+  }, [])
+
   return (
     <>
       <h1>Ships</h1>
       {ships.map(ship => (
         <div key={ship.id}>{ship.name}</div>
       ))}
-      <h1>Characters</h1>
-      {characters.map(character => (
-        <div key={character.id}>{character.name}</div>
-      ))}
     </>
   )
-}
-
-HomePage.getInitialProps = async () => {
-  const ships = await api(listShips)
-  const characters = await api(listCharacters)
-
-  return {
-    ships: get(ships, 'data.listShips.items'),
-    characters: get(characters, 'data.listCharacters.items')
-  }
-}
-
-HomePage.propTypes = {
-  ships: PropTypes.array,
-  characters: PropTypes.array
-}
-
-HomePage.defaultProps = {
-  ships: [],
-  characters: []
 }
 
 export default HomePage
